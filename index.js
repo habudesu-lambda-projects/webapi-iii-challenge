ff47e28d793f99beddf0fb50bccbe35ebd8c7682
Ken Ruf <ruf.kenichi@gmail.com>
2019-07-30 21:20:44 -0500

    logger working

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require('./server.js');
+
+server.listen(3000, () => {
+  console.log('Server running on localhost:3000')
+})
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
+server.use(logger);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.status(200).send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  let date = new Date;
+  console.log(`method: ${req.method}`, `url: http:localhost:3000${req.url}`, `timestamp: ${date}`);
+  next();
 };
 
 module.exports = server;
