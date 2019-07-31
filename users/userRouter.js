b25b749ea7edf2e9c9e39a81239cdc17e56facab
Ken Ruf <ruf.kenichi@gmail.com>
2019-07-30 22:21:02 -0500

    troubleshooting. confirmed get and post are working

--- users/userRouter.js
+++ users/userRouter.js
@@ -5,7 +5,7 @@ const Posts = require('../posts/postDb');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', validateUser, async (req, res) => {
   try {
     console.log(req.body);
     const user = await User.insert(req.body);
@@ -22,7 +22,7 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', async (req, res) => {
   try {
-    const users = await Users.get();
+    const users = await User.get();
     res.status(200).json(users);
   }
   catch(error) {
@@ -66,8 +66,14 @@ async function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   try {
-    let body = req.body;
-    console.log(body);
+    const body = req.body;
+    if(!body) {
+      res.status(400).json({ message: "missing post data" });
+    } else if(!body.name) {
+      res.status(400).json({ message: "missing required text field" });
+    } else {
+      next();
+    }
   }
   catch(error) {
     res.status(500).json(error);
